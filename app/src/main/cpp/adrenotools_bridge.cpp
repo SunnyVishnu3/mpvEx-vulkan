f790395434c74aa0186667e1049d16742ba520ab
SunnyVishnu3 <sagnikghoshmondal2@gmail.com>
2026-03-01 00:28:28 +0530

    Update adrenotools_bridge.cpp

--- app/src/main/cpp/adrenotools_bridge.cpp
+++ app/src/main/cpp/adrenotools_bridge.cpp
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-#include "adrenotools.h" 
+#include "adrenotools/driver.h" 
 
 #define LOG_TAG "AdrenoToolsBridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
