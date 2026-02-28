213c76c4bf40bc570b3ed6d5dfd3a387dd919192
SunnyVishnu3 <sagnikghoshmondal2@gmail.com>
2026-03-01 01:34:47 +0530

    Update adrenotools_bridge.cpp

--- app/src/main/cpp/adrenotools_bridge.cpp
+++ app/src/main/cpp/adrenotools_bridge.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <dlfcn.h>
 #include <android/log.h>
 #include "adrenotools/driver.h" 
 
@@ -10,16 +11,29 @@ extern "C" {
 
 JNIEXPORT jboolean JNICALL
 Java_app_marlboroadvance_mpvex_system_AdrenoTools_nativeHookDriver(
-        JNIEnv *env, jobject thiz, jstring driver_dir) {
+        JNIEnv *env, jobject thiz, jstring tmp_lib_dir, jstring hook_lib_dir, jstring custom_driver_dir) {
     
-    const char *dir_path = env->GetStringUTFChars(driver_dir, nullptr);
+    const char *tmp_dir = env->GetStringUTFChars(tmp_lib_dir, nullptr);
+    const char *hook_dir = env->GetStringUTFChars(hook_lib_dir, nullptr);
+    const char *driver_dir = env->GetStringUTFChars(custom_driver_dir, nullptr);
     
-    // Intercept Vulkan and force it to use our extracted Turnip driver
-    void* handle = adrenotools_open("libvulkan_freedreno.so", dir_path); 
+    // The REAL adrenotools interception call
+    void* handle = adrenotools_open_libvulkan(
+        RTLD_NOW, 
+        ADRENOTOOLS_DRIVER_CUSTOM, 
+        tmp_dir, 
+        hook_dir, 
+        driver_dir, 
+        "libvulkan_freedreno.so", 
+        nullptr, 
+        nullptr
+    ); 
     
-    env->ReleaseStringUTFChars(driver_dir, dir_path);
+    env->ReleaseStringUTFChars(tmp_lib_dir, tmp_dir);
+    env->ReleaseStringUTFChars(hook_lib_dir, hook_dir);
+    env->ReleaseStringUTFChars(custom_driver_dir, driver_dir);
+    
     return handle != nullptr ? JNI_TRUE : JNI_FALSE;
 }
 
 }
-
