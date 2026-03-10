1bf7b0ad1be348aef9b58f8c49bef7940f422316
SunnyVishnu3 <sagnikghoshmondal2@gmail.com>
2026-03-10 10:58:45 +0530

    Update SlideToUnlock.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/controls/components/SlideToUnlock.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/controls/components/SlideToUnlock.kt
@@ -34,7 +34,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
